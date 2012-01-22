5cd3cd0a2a6def624a7ed3a1a3ea343866029055
nottinhill <nottinhill@ecomail.at>
2012-01-22 19:21:23 +0100

    Major Release 0.2. Fixed English Search Box. 	modified:   README 	modified:   piratenkleider/header.php

--- piratenkleider/header.php
+++ piratenkleider/header.php
@@ -75,8 +75,8 @@
 				<!-- #search form -->
 					<form method="get" id="searchform" action="<?php bloginfo('home'); ?>/">
 					<div>
-						<input type="text" size="put_a_size_here" name="s" id="s" value="Search" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
-						<input type="submit" id="searchsubmit" value="Search" class="btn" />
+						<input type="text" size="12" name="s" id="s" value="Suchbegriff" onfocus="if(this.value==this.defaultValue)this.value='';" onblur="if(this.value=='')this.value=this.defaultValue;"/>
+						<input type="submit" id="searchsubmit" value="Suche" class="btn" />
 					</div>
 					</form>
 				<!-- #search form -->
